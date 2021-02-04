c4432b20910813ed8a1f05633d00a6fe130ce070
Matt Kevan <m@kevan.tv>
2021-02-04 16:07:22 +0000

    Delete zls.sh

--- zls.sh
+++
@@ -1,9 +0,0 @@
-count=$1
-stop=$2
-
-while [ $count -le $stop ]
-do
-  wget https://res.cloudinary.com/dl4abuxel/image/upload/c_scale,e_multiply,h_1090,l_$3_$count,w_1362,x_0,y_0/v1612200911/landscape-frame.jpg -O $4-$count.jpg
-  wget https://res.cloudinary.com/dl4abuxel/image/upload/c_crop,h_900,w_1200/v1588690105/$3_$count.png -O $4-$count-crop.jpg
-  count=$(( $count +1 ))
-done
